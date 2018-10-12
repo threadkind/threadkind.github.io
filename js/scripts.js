d55e7c62d8002292e52b530fadd412a52dd5a509
threadkind <threadkind@gmail.com>
2018-10-12 12:23:54 -0500

    feat: Add expanded descriptions for projects

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ const projectObj = {
 		id: 'neighborhoodMap',
 		name: 'Neighborhood Map',
 		image: 'images/projects/neighborhood-map.png',
-		description: 'Build from scratch with custom map markers as final project in Nanodegree',
+		description: 'A responsive, progressive web app built from scratch with React with accessibility considerations. This project includes custom map markers, uses the Google Maps API and is the final project in the Nanodegree program. It also pulls through data from Flickr, FourSquare + Unsplash APIs.',
 		build: 'Built with React, CSS and Javascript(JSX)',
 		url: 'https://portfolio.threadkind.com/GWGND07-Neighborhood-Map/',
 		repo: 'https://github.com/threadkind/GWGND07-Neighborhood-Map'
@@ -29,7 +29,7 @@ const projectObj = {
 		id: 'bookTrackingApp',
 		name: 'Book Tracking App',
 		image: 'images/projects/book-tracking-app.png',
-		description: 'Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'A responsive book tracking app built with React with routing using React-Router. Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship. An initial static page was provided (link to starter code on GitHub README). Books can be changed between shelves and there is a search page to add more books to the shelves. The project uses custom CSS and has a custom menu button.',
 		build: 'Built with React, HTML, CSS and Javascript',
 		url: 'http://portfolio.threadkind.com/GWGND06-MyReads-Book-Tracking-App/',
 		repo: 'https://github.com/threadkind/GWGND06-MyReads-Book-Tracking-App'
@@ -38,7 +38,7 @@ const projectObj = {
 		id: 'restaurantReviewApp',
 		name: 'Restaurant Review App',
 		image: 'images/projects/restaurant-reviews.png',
-		description: 'Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'A responsive Restaurant Review progressive web app. Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship. An initial static page was provided (link to starter code on GitHub README). The app registers a service worker and has a JSON manifest. Custom CSS was included to improve color contrast for imporved accessibility',
 		build: 'Built with HTML, CSS and Javascript with Accessibility and Service Worker',
 		url: 'https://github.com/threadkind/GWGND05-Restaurant-Review-App',
 		repo: 'https://github.com/threadkind/GWGND05-Restaurant-Review-App'
@@ -47,7 +47,7 @@ const projectObj = {
 		id: 'searchApp',
 		name: 'Search App',
 		image: 'images/projects/search-app.png',
-		description: 'Created as an exercise for the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'A responsive search app pulling through information from The New York Times API and images from Unsplash API. The initial view shows a random image from Unsplash. When a word is search the background changes to a related image, articles are pulled from the NY Times and other additional images are pulled from Unsplash.',
 		build: 'Built with HTML, CSS and Javascript with API calls to Unsplash and NY Times',
 		url: 'http://portfolio.threadkind.com/Search-App/',
 		repo: 'https://github.com/threadkind/Search-App'
@@ -56,7 +56,7 @@ const projectObj = {
 		id: 'feedReaderTesting',
 		name: 'Feed Reader Testing',
 		image: 'images/projects/feed-reader-testing.png',
-		description: 'Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'For this project I was given a web based application that reads RSS feeds. My task was to write tests using Jasmine to verify a variety of specs. Full details can be founds on GitHub README. ',
 		build: 'Test suite built with Jasmine',
 		url: 'http://portfolio.threadkind.com/GWGND04-Feed-Reader-Testing/',
 		repo: 'https://github.com/threadkind/GWGND04-Feed-Reader-Testing'
@@ -65,7 +65,7 @@ const projectObj = {
 		id: 'arcadeGameClone',
 		name: 'Arcade Game Clone',
 		image: 'images/projects/classic-arcade-game-clone.png',
-		description: 'Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'A Frogger-style web-based arcade game created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship. Some inital files were provided, such as images and a game engine. Many additional features were added such as scoring, character choice, lives, local storage etc. Full details can be found on Github README',
 		build: 'Built with HTML, CSS + (Object Oriented) Javascript',
 		url: 'http://portfolio.threadkind.com/GWGND03-Classic-Arcade-Game-Clone/',
 		repo: 'https://github.com/threadkind/GWGND03-Classic-Arcade-Game-Clone'
@@ -74,7 +74,7 @@ const projectObj = {
 		id: 'memoryGame',
 		name: 'Memory Game',
 		image: 'images/projects/memory-game.png',
-		description: 'Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'A memory game coded from scratch as a project for the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship. The game allows of a choice of multiple card packs. Best times and scores are saved in local storage for the player. I created all images for the project.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/GWGND02-Memory-Game/',
 		repo: 'https://github.com/threadkind/GWGND02-Memory-Game'
@@ -83,7 +83,7 @@ const projectObj = {
 		id: 'portfolioSite',
 		name: 'Portfolio Site',
 		image: 'images/projects/portfolio-site.png',
-		description: 'Created as part of the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship',
+		description: 'A simple reponsive portfolio site created as the initial project for the Grow With Google Udacity Front End Web Developer Nanodegree Scholarship. The project uses CSS Flexbox and media queries for the responsive views.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/GWGND01-Portfolio-Site/',
 		repo: 'https://github.com/threadkind/GWGND01-Portfolio-Site'
@@ -92,7 +92,7 @@ const projectObj = {
 		id: 'pixelArtMaker',
 		name: 'Pixel Art Maker',
 		image: 'images/projects/pixel-art-maker.png',
-		description: 'Created as part of the Udacity/Google Scholarship',
+		description: 'A pixel drawing tool coded from scratch. Upon loading the page, the user can choose a custom canvas size to start. The color picker allows the user to choose a color to draw with on the canvas.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/udacity-google/Lesson%2021/Project%20-%20Pixel%20Art%20Maker/project-pixel-art-maker-starter-master/index.html',
 		repo: 'https://github.com/threadkind/udacity-google/tree/master/Lesson%2021/Project%20-%20Pixel%20Art%20Maker/project-pixel-art-maker-starter-master'
@@ -101,7 +101,7 @@ const projectObj = {
 		id: 'tvSpinner',
 		name: 'TV Spinner',
 		image: 'images/projects/tv-spinner.png',
-		description: 'Spinner to choose what to watch on TV',
+		description: 'A spinner to choose what to watch on TV. When the "spin" button is clicked, the wheel (showing different tv viewing options) will rotate to a random point.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/tv-game/index.html',
 		repo: 'https://github.com/threadkind/tv-game'
@@ -110,7 +110,7 @@ const projectObj = {
 		id: 'bedtimeGame',
 		name: 'Bedtime Game',
 		image: 'images/projects/bedtime-game.png',
-		description: 'Bedtime routine game for children',
+		description: 'A web-based bedtime routine game for a child. When the page loads it shows all the tasks that need to be completed. The timer can be started, which gives approximate completion times. When a task is completed, the child can click the current card - it will flip to the next card and play a sound. The panel on the side will mark the card as complete. When all cards are complete the page shows confetti and plays a clapping sound. If the timer is getting close to zero a warning sound will play.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/schedule/index.html',
 		repo: 'https://github.com/threadkind/schedule'
@@ -119,7 +119,7 @@ const projectObj = {
 		id: 'productSite',
 		name: 'Product Site',
 		image: 'images/projects/product-site.jpg',
-		description: 'A product site showcasing various courses offered by a company.',
+		description: 'A simple mock up of a mobile reponsive product site showcasing various courses offered by a company. The hamburger menu can be used to filter courses by category.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/30days30sites/04ProductSite/index.html',
 		repo: 'https://github.com/threadkind/30days30sites'
@@ -128,7 +128,7 @@ const projectObj = {
 		id: 'touristAttractionSite',
 		name: 'Tourist Attraction Site',
 		image: 'images/projects/tourist-attraction.jpg',
-		description: 'A responsive site showing information about Mount Rushmore.',
+		description: 'A responsive mock up of a tourist information website showing information about Mount Rushmore. When a section is hovered over, it expands and provides more information about that category. The other categories widths decrease to provide more space for the chosen section. CSS Flexbox aids with the functionality of the site.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/30days30sites/03TouristAttraction/index.html',
 		repo: 'https://github.com/threadkind/30days30sites'
@@ -137,7 +137,7 @@ const projectObj = {
 		id: 'bedtimeGame',
 		name: 'Restaurant Menu',
 		image: 'images/projects/menu.jpg',
-		description: 'A mock-up of a responsive restaurant menu website.',
+		description: 'A mock-up of a responsive restaurant menu website. When a food category is clicked, that section expands to show the mock menu items available. It can be clicked again to collapse the section.',
 		build: 'Built with HTML, CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/30days30sites/05RestaurantMenu/index.html',
 		repo: 'https://github.com/threadkind/30days30sites'
@@ -146,7 +146,7 @@ const projectObj = {
 		id: 'codevember',
 		name: 'Codevember 2017',
 		image: 'images/projects/codevember.png',
-		description: 'CodeVember is a challenge for developers to make a creative sketch a day in November',
+		description: 'CodeVember is a challenge for developers to make a creative sketch a day in November. I coded a number of sketches as coding projects, the index can be found linked here. The sketches are available to view on Github and Codepen.',
 		build: 'Built with HTML (HTML5 Canvas), CSS + Javascript',
 		url: 'http://portfolio.threadkind.com/codevember2017/index.html',
 		repo: 'https://github.com/threadkind/codevember2017'
@@ -167,8 +167,10 @@ for (let i = 0; i < Object.values(projectObj).length; i++){
             <img src="${image}">
             <a href="${url}" target="_blank">
               <div class="projectInfo hide">
-                <h2>${name}</h2><br>
+                <h2>${name}</h2>
+                <div class="info-spacer"></div>
                 <p>${description}</p>
+                <div class="info-spacer"></div>
                 <p>${build}</p>
               </div>
             <div class="overlay"></div>
